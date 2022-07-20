0a56b6a66b5d8c6878b92edf001a7707ce303bdc
DiegoFabbrii <diegofabbri@outlook.com>
2022-07-19 23:00:49 -0300

    grid template columns changes

--- src/components/Main/LatestArticles/ArticlesCard/styles.js
+++ src/components/Main/LatestArticles/ArticlesCard/styles.js
@@ -4,7 +4,7 @@ const { colors } = styleGuide;
 
 export const Container = styled.div`
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+    grid-template-columns: repeat(auto-fit, minmax(230px, 1fr));
     gap: 20px;
 `;
 
